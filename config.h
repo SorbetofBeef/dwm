c2b3146b823741a7a64961fe0fc3c03a32685048
SorbetofBeef <christianadickerson@protonmail.com>
2020-12-13 16:14:42 +0000

    Merged old configs

--- config.h
+++ config.h
@@ -1,41 +1,70 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  =  3;        /* border pixel of windows */
+static const unsigned int snap      =  2;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
+static       int smartgaps          =  0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            =  1;        /* 0 means no bar */
+static const int topbar             =  1;        /* 0 means bottom bar */
+static const int vertpad            =  5;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "InconsolataGo Nerd Font:size=12" };
+static const char dmenufont[]       =   "InconsolataGo Nerd Font:size=14";
+/* colors */
+static const char col_fg[]	        = "#cbe3e7";
+static const char col_bg[]          = "#1e1c31";
+/* Row for kitty copypasta */
+/* black */                                   
+static const char col_black0[]      = "#565575";
+static const char col_black8[]      = "#100e23";
+/* red */                                     
+static const char col_red1[]        = "#ff8080";
+static const char col_red9[]        = "#ff5458";
+/* green */                                   
+static const char col_green2[]      = "#95ffa4";
+static const char col_green10[]     = "#62d196";
+/* yellow */                                  
+static const char col_yellow3[]     = "#ffe9aa";
+static const char col_yellow11[]    = "#ffb378";
+/* blue */                                    
+static const char col_blue4[]       = "#91ddff";
+static const char col_blue12[]      = "#65b2ff";
+/* magenta */                                 
+static const char col_magenta5[]    = "#c991e1";
+static const char col_magenta13[]   = "#906cff";
+/* cyan */                                    
+static const char col_cyan6[]       = "#aaffe4";
+static const char col_cyan14[]      = "#63f2f1";
+/* white */                                   
+static const char col_white7[]      = "#cbe3e7";
+static const char col_white15[]     = "#a6b3cc";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg           bg               border   */
+	[SchemeNorm] = { col_cyan14,  col_black8,     col_black0 },
+	[SchemeSel]  = { col_black8,  col_magenta5,  col_blue12  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { " 1   ", " 2   ", " 3   ", " 4   ", " 5   ", " 6   ", " 7   ", " 8   "};
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1,        50,50,500,500,        5 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,        50,50,500,500,        5 },
+	/* class      instance    title       tags mask     isfloating   monitor  float x,y,w,h         floatborderpx*/
+  { "kitty",    NULL,       "project",    1 << 1,       0,           -1,    50,50,500,500,        5 },
+  { "kitty",    NULL,       "gh-cli",     1 << 2,       1,           -1,    50,50,500,500,        5 },
+	{ "Slack",    NULL,       NULL,         1 << 4,       0,           -1,    50,50,500,500,        5 },
+	{ "zoom",     NULL,       NULL,         1 << 5,       1,           -1,    50,50,500,500,        5 },
+	{ "kitty",    NULL,       "calcurse",   1 << 6,       0,           -1,    50,50,500,500,        5 },
+	{ "kitty",    NULL,       "neomutt",    1 << 6,       0,           -1,    50,50,500,500,        5 },
+	{ "kitty",    NULL,       "newsboat",   1 << 6,       0,           -1,    50,50,500,500,        5 },
+	{ "kitty",    NULL,       "gotop",      1 << 7,       1,           -1,    50,50,500,500,        5 },
 };
 
 /* layout(s) */
@@ -76,14 +105,21 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, 
+
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
-static const char *rangercmd[]  = { "kitty", "ranger", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_cyan14, "-nf", col_black0, "-sb", col_magenta13, "-sf", col_cyan6, NULL };
+static const char *termcmd[]      = { "kitty", NULL };
+static const char *chromiumcmd[]  = { "chromium", NULL };
+static const char *slackcmd[]     = { "slack", NULL };
+static const char *zoomcmd[]      = { "zoom", NULL };
+static const char *gotopcmd[]     = { "kitty", "--title", "gotop", "gotop", NULL };
+static const char *nvimcmd[]      = { "kitty", "--title", "nvim", "nvim", NULL };
+static const char *calcursecmd[]  = { "kitty", "--title", "calcurse", "calcurse", NULL };
+static const char *rangercmd[]    = { "kitty", "--title", "ranger", "ranger", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
